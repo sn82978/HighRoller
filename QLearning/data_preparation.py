6314b9994ddf948451cbf75c08979595f686bbc0
Shreya Nakum <shreyanakum2@gmail.com>
2026-08-14 11:29:31 -0700

    data_prep is done

---
+++ QLearning/data_preparation.py
@@ -0,0 +1,39 @@
+'''
+take the df of all market rows, clean the timestamps, and filter out neg pre-market candles
+
+make a list of df's w/ one 15-minute market
+'''
+
+import pandas as pd
+import os
+
+DATA_DIR = "/Users/shreyanakum/Documents/HighRoller/data/polymarket/btc_updown_15m_candles_15s"
+
+def prepare_episodes(df: pd.DataFrame):
+    episodes = []
+
+    df = df.sort_values(["event_slug", "t"]).copy()
+
+    for i, ep_df in df.groupby("event_slug"):
+        ep_df = ep_df[ep_df["candle_index"] >= 0].copy() # before the market is when the candle_index is less than 0
+
+        # print(ep_df.head())
+
+        if ep_df.empty:
+            print("empty")
+            continue
+
+        ep_df["price_up"] = ep_df["close"] # ir we buy_up, entry price is price_up 
+        ep_df["price_down"] = 1.0 - ep_df["price_up"] # if we buy_down, entry price is price_down
+
+        ep_df = ep_df.reset_index(drop=True) # iloc indexing in env matches exactly
+        episodes.append(ep_df)
+
+    return episodes
+
+# for file in os.listdir(DATA_DIR):
+#     if not file.endswith('.csv'):
+#         continue
+df = pd.read_csv(f"{DATA_DIR}/btc_updown_15m_candles_15s_2025-11-28.csv")
+episodes = prepare_episodes(df)
+print(f"{len(episodes)} episodes")
